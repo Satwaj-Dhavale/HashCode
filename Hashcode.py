f676cd160125c4404d7f60a57c30a6768848a5a1
Satwaj Dhavale <thesatwaj@gmail.com>
2020-02-17 21:07:48 +0530

    Update Hashcode.py

--- Hashcode.py
+++ Hashcode.py
@@ -1,54 +1,68 @@
 def solution(slices, slices_per_type):
 
-    selectedIndex = []
-    selectedValue = []
-    solIndex = []
 
-    size_of_problem = len(slices_per_type)
-    start_index = size_of_problem
 
-    sum = 0
-    max_sum = 0
-    while ((len(selectedIndex)>0 and selectedIndex[0] != 0) or len(selectedIndex) == 0):
-        start_index = start_index - 1
+M = x[0]
+N = len(slices_per_type)
 
-        for i in range(start_index, -1, -1):
-            current_slice = slices_per_type[i]
-            temp = sum + current_slice
-            if (temp == slices):
-                sum = temp
-                selectedIndex.append(i)
-                selectedValue.append(current_slice)
-                break
-            elif (temp > slices):
-                continue
-            elif (temp < slices):
-                sum = temp
-                selectedIndex.append(i)
-                selectedValue.append(current_slice)
-        if (max_sum < sum):
-            max_sum = sum
-            solIndex = []
-            solValue = []
+selectedIndex = []
+selectedValue = []
+solIndex = []
 
-            for y in selectedIndex:
-                solIndex.append(y)
-            for y in selectedValue:
-                solValue.append(y)
+start_index = -1
+S = 0
+max_sum = 0
 
-        if (max_sum == slices):
+#start_index 
+while ((len(selectedIndex)>0 and selectedIndex[0] != N-1) or len(selectedIndex) == 0):
+    start_index = start_index + 1
+    
+    for i in range(start_index,N):
+        current_slice = slices_per_type[i]
+        #add the slice into temp
+        temp = S + current_slice
+
+        #check temp for following 3 conditions
+        if (temp == M):
+            S = temp
+            selectedIndex.append(i)
+            selectedValue.append(current_slice)
             break
-        if (len(selectedValue) != 0):
-            last_val = selectedValue.pop()
-            sum = sum - last_val
-        if (len(selectedIndex) != 0):
-            last_index = selectedIndex.pop()
-            start_index = last_index
-        if((len(selectedIndex) == 0 and (start_index == 0))):
-            break
-    print("Max sum:",max_sum)
-    return(solIndex[::-1])
+        elif (temp > M):
+            continue
+        elif (temp < M):
+            S = temp
+            selectedIndex.append(i)
+            selectedValue.append(current_slice)
+   
+    if (max_sum < S):
+        max_sum = S
+        #clear the list
+        solIndex = []
+        solValue = []
 
+        for i in selectedIndex:
+            solIndex.append(i)
+        for v in selectedValue:
+            solValue.append(v)
+
+    if (max_sum == M):
+        break
+    
+    if (len(selectedValue) != 0):
+        last_val = selectedValue.pop()
+        S = S - last_val
+        
+    if (len(selectedIndex) != 0):
+        last_index = selectedIndex.pop()
+        start_index = last_index
+   
+    if((len(selectedIndex) == 0 and (start_index == N-1))):
+        break
+    
+    return(solIndex) 
+
+ 
 def write_output(file):
     input_file = open(input_directory + file + '.in', 'r')
     firstline = input_file.readline()
